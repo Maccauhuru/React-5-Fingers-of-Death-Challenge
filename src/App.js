b14b671afd958f85f541186569a9b43b6462978f
S.L.Mupfukudzwa <simba3310@gmail.com>
2018-05-24 21:06:49 -0500

    Greeting function class component added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>React App!</h1>
-          <h1 className="App-title">Welcome to React 101</h1>
+          <Greetings />
         </header>
         
       </div>
     );
   }
 }
+
+
+function Greetings (){
+    return (
+      <div>
+        <h1 className="App-title">Welcome to React 101</h1>
+      </div>
+    )
+
+  }
+  
+
 
 export default App;
