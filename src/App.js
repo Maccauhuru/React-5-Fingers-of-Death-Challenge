1aada370c903d5c9cf82f8fbea2f01c03b52c60b
S.L.Mupfukudzwa <simba3310@gmail.com>
2018-05-25 16:09:10 -0500

    added style to div and created new Header component

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>React App!</h1>
-          <Greetings />
+          <Greetings welcomeMsg="Welcome to React 101" />
+          <Header />
         </header>
-        
       </div>
     );
   }
 }
 
+const Greetings = ({ welcomeMsg }) =>
+  <div style={{ color: '#fff', backgroundColor: 'red' }}>
+    <h1 className="App-title">{welcomeMsg}</h1>
+  </div>;
 
-function Greetings (){
-    return (
-      <div>
-        <h1 className="App-title">Welcome to React 101</h1>
-      </div>
-    )
-
-  }
-  
-
+const Header = () =>
+  <div style={{color: 'white'}}>
+    <a>Home</a> | <a>About</a> | <a>Contact</a>
+  </div>;
 
 export default App;
