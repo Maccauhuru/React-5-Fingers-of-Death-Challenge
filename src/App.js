2869dee3276728dfc456844dbbd665ac3c947d56
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-05 16:45:06 -0500

    New func and class componenets for multiple render in index.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>React App!</h1>
-          <Greetings welcomeMsg="Welcome to React 101" />
-          <Header />
-        </header>
-      </div>
-    );
-  }
+import React from 'react';
+class ParentComponent extends React.Component {
+render(){
+return(<React.Fragment>
+  <h1>Basic React Component {200-99}</h1>
+  <p>{ < CalcComponent /> }</p>
+  <BodyText />
+  </React.Fragment>);
+}
 }
 
-const Greetings = ({ welcomeMsg }) =>
-  <div style={{ color: '#fff', backgroundColor: 'red' }}>
-    <h1 className="App-title">{welcomeMsg}</h1>
-  </div>;
+const BodyText =()=>
 
-const Header = () =>
-  <div style={{color: 'white'}}>
-    <a>Home</a> | <a>About</a> | <a>Contact</a>
-  </div>;
+<div>
+Every man who has lotted here over the centuries, has looked up to the light and imagined climbing to freedom.
+So easy, so simple! And like shipwrecked men turning to seawater foregoing uncontrollable thirst,
+many have died trying. And then here there can be no true despair without hope.
+So as I terrorize Gotham, I will feed its people hope to poison their souls.
+I will let them believe that they can survive so that you can watch them climbing over each other to stay in the sun.
+You can watch me torture an entire city.
+And then when you’ve truly understood the depth of your failure, we will fulfill Ra’s Al Ghul’s destiny.
+We will destroy Gotham. And then, when that is done, and Gotham is… ashes Then you have my permission to die.
+</div>
 
-export default App;
+
+const CalcComponent = () =>100 * 1000;
+
+export default ParentComponent;
