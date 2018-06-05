1837058e0cfb072598551d5632e528feace9ee9c
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-05 16:45:52 -0500

    used react.Fragment for multiple component rendering

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.Fragment>
+  <App /><hr />
+  <App />
+  </React.Fragment>, document.getElementById('root'));
 registerServiceWorker();
