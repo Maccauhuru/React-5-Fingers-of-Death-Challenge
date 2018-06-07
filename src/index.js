5496687e15c8b85fe8bb543244df9ea7b9d3c799
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-06 19:00:51 -0500

    use react fragments to rencer multiple components in a single div

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import PropsValidation from './PropsValidation';
 
 ReactDOM.render(
   <React.Fragment>
-  <App /><hr />
-  <App />
+  <App headerText="Getting started with Props in React16" /><hr />
+  <PropsValidation name="Wu Muh"/>
   </React.Fragment>, document.getElementById('root'));
 registerServiceWorker();
