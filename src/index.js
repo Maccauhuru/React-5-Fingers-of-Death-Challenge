a88f7d3b88f08bcfa6a2c7069da7bb7696ba48f4
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-15 14:12:10 -0500

    React lifecycle changed states logging in console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import PropsValidation from './PropsValidation';
-import EventHandling from './EventHandling';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import PropsValidation from './PropsValidation';
+// import EventHandling from './EventHandling';
+// registerServiceWorker();
+//import HelloWorld from './HelloWorld';
+import StateChanges from './StateChanges';
 
 ReactDOM.render(
-<EventHandling>Text From Above</EventHandling>, document.getElementById('root'));
-registerServiceWorker();
+<StateChanges />, document.getElementById('root'));
+
