87566e15ab551429376d5678185b81b7db9cbcb4
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-22 11:38:55 -0500

    changed

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,12 +1,9 @@
 import React , { Component } from 'react';
 
 class HelloWorld extends Component {
-  state = {
-    tech : 'REDUX',
-    year : 2018
-  }
+
   render(){
-    return(<div>Say Hello To My Lil Friend {this.state.tech} in the year {this.state.year}</div>)
+    return(<div>Say Hello To My Lil Friend </div>)
   }
 }
 
