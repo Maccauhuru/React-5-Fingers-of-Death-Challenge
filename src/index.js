3b0303a202ace51a6ab936c5401bc831142ba15d
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-24 16:39:35 -0500

    new componenets to render

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import ReactListsCollections from './components/ReactListsCollections';
 // import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 // import PropsValidation from './PropsValidation';
 // import EventHandling from './EventHandling';
 // registerServiceWorker();
 //import HelloWorld from './HelloWorld';
-//import StateChanges from './StateChanges';
+//import StateChanges from './components/StateChanges';
 //import ReactForms from './components/ReactForms';
 //import HelloWorldPureReactBook from './components/HelloWorldPureReactBook';
 //import HelloPureReactBook from './components/HelloPureReactBook';
 //import WorldPureReactBook from './components/WorldPureReactBook';
 //import ReactListsCollections from './components/ReactListsCollections'
-import HelloRedux from './components/HelloRedux';
+//import HelloRedux from './components/HelloRedux';
+//import ReactRouter from './components/ReactRouter';
+//import ReactAnimations from './components/ReactAnimations';
 
 
-ReactDOM.render( <HelloRedux / > , document.getElementById('root'));
+ReactDOM.render( <ReactListsCollections /> , document.getElementById('root'));
 
