482e9ba06ef371a8204c3b80f32425979002901d
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-28 14:14:34 -0500

    app state now managed by redux instead of components

--- src/components/HelloRedux.js
+++ src/components/HelloRedux.js
@@ -1,12 +1,17 @@
 import React , { Component } from 'react';
 import { createStore } from 'redux';
+import reducer from '../reducers/index';
 
-const store = createStore(reducer);
+const initialState={
+  tech : 'Redux Rocks',
+  year : 2018
+}
+const store = createStore(reducer,initialState);
 
 class HelloRedux extends Component {
-
+  //state = store.getState();
   render(){
-    return(<div>Say Hello To My Lil Friend {this.state.tech} in the year {this.state.year}</div>)
+    return(<div>Say Hello To My Lil Friend {store.getState().tech} in the year {store.getState().year}</div>)
   }
 }
 
