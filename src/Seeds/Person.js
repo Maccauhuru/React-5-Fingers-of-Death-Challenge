0c9980fce1f6e91501e752a00406d91662dbec4e
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-24 20:47:38 -0500

    updated obj

---
+++ src/Seeds/Person.js
@@ -0,0 +1,16 @@
+const Person = [
+  {
+    fullName: "Wesley Snipes",
+    Address: "123 Fake St.",
+    State: "Boston",
+    Zip: "02118"
+  },
+  {
+    fullName: "Michael Jordan",
+    Address: "456 Fake St.",
+    State: "Chicago",
+    Zip: "56236"
+  }
+];
+
+export default Person;
