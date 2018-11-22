1286689f01b112d714a6e794c3b2be88499fa266
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-11-21 20:00:13 -0600

    createReactElment

--- src/components/AddressLabel.js
+++ src/components/AddressLabel.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 export default class AddressLabel extends Component {
   render() {
-      console.log(this.props);
     return (
       <div>
       {this.props.info["0"].Address}
